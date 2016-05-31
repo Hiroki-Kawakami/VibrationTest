9defb9183a772aadc0d121807ff9c7424c63b099
川上大貴 <hiroki_common@me.com>
2016-05-31 19:32:18 +0900

    Initial Commit

---
+++ VibrationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VibrationTest
+//
+//  Created by hiroki on 2016/05/31.
+//  Copyright © 2016年 hiroki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
